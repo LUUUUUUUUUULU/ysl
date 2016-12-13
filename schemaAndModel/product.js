5c089f49a2ad1631e404e73db6ec2e1b88d741bd
LUUUUUUUUUULU <ll.lee@live.cn>
2016-12-13 11:36:00 +0800

    change some routes and model lulu

--- schemaAndModel/product.js
+++ schemaAndModel/product.js
@@ -17,35 +17,14 @@ var productSchema = {
   sliderpictures: [{ type: String, match: /^http:\/\//i }],
   //options part
   priceoptions: [{ type: Number}],
-  flavoroptions: [{ type: String}],
-  selectedflavor:{ type: String},
   sizeoptions: [{ type: String}],
   selectedsize:{ type: String},
   //descrition part
   shortDes:{ type: String},
   detailDes: [{ type: String }],
-  flavor:{ type: String},
-  expirationDate:{ type: String},
-  eatmethod:{ type: String},
-  savemethod:{ type: String},
-  weight:{ type: String},
-  mifang:{ type: String},
-  requirement:{ type: String},
+
   category: Category.categorySchema,
-    //promotion part
-    /*
-  promote:{
-  prolist:  [{
-   type:Number ,
-   ref:'Product'
-    }], // products promoted with the product
-    method: { type: String} //promote methed ex: fix price
-  }, 
-  */
-  comments: [{ 
-            type:Schema.ObjectId ,
-            ref:'Comment'
-                    }]
+
 };
 module.exports = new mongoose.Schema(productSchema);
 module.exports.productSchema = productSchema;
